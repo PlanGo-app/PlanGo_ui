e48df2a1ab54e52cf763433753685a132c0e522f
Gael <gael.brct@gmail.com>
2021-10-18 13:09:17 +0200

    Begin welcom page, and set first file tree

--- front/plango_front/lib/main.dart
+++ front/plango_front/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:plango_front/views/login/connexion_page.dart';
+import 'package:plango_front/views/welcome_page//welcome_page.dart';
+import 'views/header_bar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,21 +14,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Plango',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: Container(
-        
+      home: const Scaffold(
+        body: WelcomePage(),
       ),
     );
   }
