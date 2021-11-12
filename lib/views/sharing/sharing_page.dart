d6ad47a3c60671c9bd0855affc83b3d6f388da03
Anthony Lafolie <anthony.lafolie.etu@univ-lille.fr>
2021-11-12 12:16:30 +0100

    add design for sharing page

--- lib/views/sharing/sharing_page.dart
+++ lib/views/sharing/sharing_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:plango_front/util/constant.dart';
 import 'package:plango_front/views/components/rounded_button.dart';
 import 'package:plango_front/views/travels_list/travels_list_background.dart';
 
@@ -9,41 +11,66 @@ class SharingPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return BackgroundTravelsList(
       child: Column(
-        // mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Flexible(
-            flex: 7,
-            child: Container(
-              color: Colors.redAccent,
-            ),
-          ),
-          Flexible(
-            flex: 3,
+          Container(
+            height: 600,
             child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Container(
-                  // color: Colors.blueAccent,
-                  // height: double.infinity,
-                  width: MediaQuery.of(context).size.width / 2,
-                  child: RoundedButton(
-                      text: "Retour",
-                      press: () {
-                        Navigator.pop(context);
-                      }),
+                  margin: const EdgeInsets.symmetric(vertical: 10),
+                  width: 240,
+                  child: Card(
+                    child: TextFormField(
+                      initialValue: "Code de partage",
+                      readOnly: true,
+                      decoration: const InputDecoration(
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: kPrimaryColor),
+                        ),
+                      ),
+                    ),
+                  ),
                 ),
-                Container(
-                  // color: Colors.green,
-                  // height: double.infinity,
-                  width: MediaQuery.of(context).size.width / 2,
-                  child: RoundedButton(
-                      text: "Partager",
-                      press: () {
-                        print("Partager");
-                      }),
-                ),
+                Card(
+                    child: IconButton(
+                        icon: Icon(Icons.copy),
+                        onPressed: () {
+                          Clipboard.setData(
+                              new ClipboardData(text: "Code de partage"));
+                        })),
               ],
             ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                // color: Colors.blueAccent,
+                // height: double.infinity,
+                width: MediaQuery.of(context).size.width / 2,
+                child: RoundedButton(
+                    text: "Retour",
+                    press: () {
+                      Navigator.pop(context);
+                    }),
+              ),
+              Container(
+                // color: Colors.green,
+                // height: double.infinity,
+                width: MediaQuery.of(context).size.width / 2,
+                child: RoundedButton(
+                    text: "Partager",
+                    press: () {
+                      print("Partager");
+                    }),
+              ),
+            ],
           ),
         ],
       ),
