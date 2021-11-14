256300c207ce01b69b43e16b78231c1554c0da5a
Anthony Lafolie <anthony.lafolie.etu@univ-lille.fr>
2021-11-14 17:27:39 +0100

    add sharing menu on b utton click

--- lib/views/sharing/sharing_page.dart
+++ lib/views/sharing/sharing_page.dart
@@ -3,76 +3,84 @@ import 'package:flutter/services.dart';
 import 'package:plango_front/util/constant.dart';
 import 'package:plango_front/views/components/rounded_button.dart';
 import 'package:plango_front/views/travels_list/travels_list_background.dart';
+import 'package:share/share.dart';
 
 class SharingPage extends StatelessWidget {
   const SharingPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return BackgroundTravelsList(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Container(
-            height: 600,
-            child: Row(
+    return Scaffold(
+      body: Builder(
+        builder: (BuildContext context) {
+          return BackgroundTravelsList(
+            child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Container(
-                  margin: const EdgeInsets.symmetric(vertical: 10),
-                  width: 240,
-                  child: Card(
-                    child: TextFormField(
-                      initialValue: "Code de partage",
-                      readOnly: true,
-                      decoration: const InputDecoration(
-                        enabledBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Colors.grey),
-                        ),
-                        focusedBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: kPrimaryColor),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.symmetric(vertical: 10),
+                      width: 240,
+                      child: Card(
+                        child: TextFormField(
+                          initialValue: "Code de partage",
+                          readOnly: true,
+                          decoration: const InputDecoration(
+                            enabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.grey),
+                            ),
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: kPrimaryColor),
+                            ),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ),
-                Card(
-                    child: IconButton(
-                        icon: Icon(Icons.copy),
+                    Card(
+                      child: IconButton(
+                        icon: const Icon(Icons.copy),
                         onPressed: () {
                           Clipboard.setData(
-                              new ClipboardData(text: "Code de partage"));
-                        })),
+                                  const ClipboardData(text: "Code de partage"))
+                              .then(
+                            (_) {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                  const SnackBar(content: Text("Code copié")));
+                            },
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: MediaQuery.of(context).size.width / 2,
+                      child: RoundedButton(
+                          text: "Retour",
+                          press: () {
+                            Navigator.pop(context);
+                          }),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width / 2,
+                      child: RoundedButton(
+                          text: "Partager",
+                          press: () {
+                            Share.share("Plango: Code a partager");
+                          }),
+                    ),
+                  ],
+                ),
               ],
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                // color: Colors.blueAccent,
-                // height: double.infinity,
-                width: MediaQuery.of(context).size.width / 2,
-                child: RoundedButton(
-                    text: "Retour",
-                    press: () {
-                      Navigator.pop(context);
-                    }),
-              ),
-              Container(
-                // color: Colors.green,
-                // height: double.infinity,
-                width: MediaQuery.of(context).size.width / 2,
-                child: RoundedButton(
-                    text: "Partager",
-                    press: () {
-                      print("Partager");
-                    }),
-              ),
-            ],
-          ),
-        ],
+          );
+        },
       ),
     );
   }
