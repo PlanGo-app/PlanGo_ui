aece684b72248f21f6ae57155fd90e0b9b254eda
Anthony Lafolie <anthony.lafolie.etu@univ-lille.fr>
2021-11-27 00:42:32 +0100

    test drag and drag target

--- lib/views/calendar/calendar.dart
+++ lib/views/calendar/calendar.dart
@@ -3,7 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:plango_front/views/nav_bar/nav_bar_bloc/nav_bar_bloc.dart';
 
 class Calendar extends StatefulWidget {
-  Calendar({Key? key}) : super(key: key);
+  const Calendar({Key? key}) : super(key: key);
 
   @override
   _CalendarState createState() => _CalendarState();
@@ -14,26 +14,115 @@ class _CalendarState extends State<Calendar> {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (_) => NavBarBloc(),
-      child: Scaffold(
-        body: body(),
-        // bottomNavigationBar: NavBar(
-        //   onList: true,
-        // ),
+      child: const Scaffold(
+        body: Body(),
       ),
     );
   }
 }
 
-class body extends StatelessWidget {
-  const body({
+class Body extends StatefulWidget {
+  const Body({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<Body> createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  List dragItems = [];
+  Widget? widgets;
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.purpleAccent,
-      child: Center(child: Text("Calendrier")),
+    return Column(
+      children: [
+        Expanded(
+          child: Container(),
+          flex: 3,
+        ),
+        Expanded(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Draggable(
+                child: Container(
+                  height: 50,
+                  width: 50,
+                  color: Colors.red,
+                ),
+                feedback: Container(
+                  height: 50,
+                  width: 50,
+                  color: Colors.green,
+                ),
+                childWhenDragging: Container(),
+              ),
+              Draggable(
+                child: Container(
+                  height: 50,
+                  width: 50,
+                  color: Colors.red,
+                ),
+                feedback: Container(
+                  height: 50,
+                  width: 50,
+                  color: Colors.green,
+                ),
+                childWhenDragging: Container(),
+                data: "Data",
+              ),
+              Draggable(
+                child: Container(
+                  height: 50,
+                  width: 50,
+                  color: Colors.red,
+                ),
+                feedback: Container(
+                  height: 50,
+                  width: 50,
+                  color: Colors.green,
+                ),
+                childWhenDragging: Container(),
+              ),
+            ],
+          ),
+          flex: 2,
+        ),
+        Expanded(
+          child: Container(),
+          flex: 3,
+        ),
+        Flexible(
+          flex: 2,
+          child: DragTarget(
+            builder: (context, List<Object?> candidateData, rejectedData) {
+              print(candidateData);
+              return Container(
+                width: 100,
+                height: 100,
+                color: candidateData.isEmpty ? Colors.blue : Colors.yellow,
+                child: const Center(
+                  child: Text(
+                    "10h00",
+                    style: TextStyle(color: Colors.black, fontSize: 22.0),
+                  ),
+                ),
+              );
+            },
+            onWillAccept: (data) {
+              print("WILL ACCEPT");
+              print(data);
+              return data == "Data";
+            },
+            onAccept: (Widget w) {
+              print("ACCEPT");
+              widgets = w;
+            },
+          ),
+        ),
+      ],
     );
   }
 }
