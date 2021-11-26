f07e97cb77cf20bc7e5f6d588d5aa51fa4869def
Anthony Lafolie <anthony.lafolie.etu@univ-lille.fr>
2021-11-26 09:21:52 +0100

    fix Rendered error with Padding

--- lib/views/nav_bar/search_text_field.dart
+++ lib/views/nav_bar/search_text_field.dart
@@ -16,77 +16,75 @@ class _SearchTextFieldState extends State<SearchTextField> {
   final searchController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        mainAxisSize: MainAxisSize.min,
-        verticalDirection: VerticalDirection.up,
-        children: [
-          searchController.text.isNotEmpty
-              ? FutureBuilder<List<Place>>(
-                  future: getPlaces(searchController.text),
-                  builder: (BuildContext context,
-                      AsyncSnapshot<List<Place>> snapshot) {
-                    switch (snapshot.connectionState) {
-                      case ConnectionState.waiting:
-                        return const CircularProgressIndicator();
-                      default:
-                        if (snapshot.hasError) {
-                          return Text('Error: ${snapshot.error}');
-                        } else if (snapshot.data!.isEmpty) {
-                          return const Text(
-                              'Aucun lieu ne correspond à votre recherche');
-                        } else {
-                          return ConstrainedBox(
-                            constraints: const BoxConstraints(maxHeight: 350),
-                            child: ListView.separated(
-                                primary: false,
-                                separatorBuilder: (context, _) => const Divider(
-                                      color: Colors.grey,
-                                      thickness: 1,
-                                    ),
-                                shrinkWrap: true,
-                                itemCount: snapshot.data!.length,
-                                itemBuilder: (context, index) {
-                                  return ListTile(
-                                    title:
-                                        Text(snapshot.data![index].displayName),
-                                    onTap: () {
-                                      context.read<NavBarBloc>().add(
-                                          NavBarEventPlaceFound(
-                                              place: snapshot.data![index]));
-                                      searchController.text = "";
-                                      FocusScope.of(context)
-                                          .requestFocus(FocusNode());
-                                    },
-                                  );
-                                }),
-                          );
-                        }
-                    }
-                  })
-              : Container(),
-          Container(
-            decoration: const BoxDecoration(
-                border: Border(top: BorderSide(color: Colors.black, width: 2))),
-            child: Flexible(
-                child: TextField(
-              // autofocus: true,
-              decoration: new InputDecoration(
-                hintText: 'Votre recherche',
-                suffixIcon: Icon(Icons.search),
-                contentPadding: EdgeInsets.all(20),
-              ),
-              controller: searchController,
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      mainAxisSize: MainAxisSize.min,
+      verticalDirection: VerticalDirection.up,
+      children: [
+        searchController.text.isNotEmpty
+            ? FutureBuilder<List<Place>>(
+                future: getPlaces(searchController.text),
+                builder: (BuildContext context,
+                    AsyncSnapshot<List<Place>> snapshot) {
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.waiting:
+                      return const CircularProgressIndicator();
+                    default:
+                      if (snapshot.hasError) {
+                        return Text('Error: ${snapshot.error}');
+                      } else if (snapshot.data!.isEmpty) {
+                        return const Text(
+                            'Aucun lieu ne correspond à votre recherche');
+                      } else {
+                        return ConstrainedBox(
+                          constraints: const BoxConstraints(maxHeight: 350),
+                          child: ListView.separated(
+                              primary: false,
+                              separatorBuilder: (context, _) => const Divider(
+                                    color: Colors.grey,
+                                    thickness: 1,
+                                  ),
+                              shrinkWrap: true,
+                              itemCount: snapshot.data!.length,
+                              itemBuilder: (context, index) {
+                                return ListTile(
+                                  title:
+                                      Text(snapshot.data![index].displayName),
+                                  onTap: () {
+                                    context.read<NavBarBloc>().add(
+                                        NavBarEventPlaceFound(
+                                            place: snapshot.data![index]));
+                                    searchController.text = "";
+                                    FocusScope.of(context)
+                                        .requestFocus(FocusNode());
+                                  },
+                                );
+                              }),
+                        );
+                      }
+                  }
+                })
+            : Container(),
+        Container(
+          // decoration: const BoxDecoration(
+          //     border: Border(top: BorderSide(color: Colors.black, width: 2))),
+          child: Flexible(
+              child: TextField(
+            // autofocus: true,
+            decoration: const InputDecoration(
+              hintText: 'Votre recherche',
+              suffixIcon: Icon(Icons.search),
+              contentPadding: EdgeInsets.all(20),
+            ),
+            controller: searchController,
 
-              onChanged: (text) {
-                setState(() {});
-                // getPlaces(searchController.text);
-              },
-            )),
-          ),
-        ],
-      ),
+            onChanged: (text) {
+              setState(() {});
+              // getPlaces(searchController.text);
+            },
+          )),
+        ),
+      ],
     );
   }
 
